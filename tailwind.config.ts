cce7587c111fe98bd5facecee7973ad8671c32c3
Humoyun Abduqodirov <abduodirovhumoyun@gmail.com>
2024-05-27 11:59:44 +0500

    fix new bugs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,39 +8,8 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
+  theme: {},
   darkMode: "class",
-  plugins: [
-    nextui({
-      prefix: "nextui", // prefix for themes variables
-      addCommonColors: false, // override common colors (e.g. "blue", "green", "pink").
-      defaultTheme: "light", // default theme from the themes object
-      defaultExtendTheme: "light", // default theme to extend on custom themes
-      layout: {}, // common layout tokens (applied to all themes)
-      themes: {
-        light: {
-          layout: {}, // light theme layout tokens
-          colors: {
-            primary: "#EFEFF4",
-          }, // light theme colors
-        },
-        dark: {
-          layout: {}, // dark theme layout tokens
-          colors: {
-            primary: "#2B2B2B",
-          }, // dark theme colors
-        },
-        // ... custom themes
-      },
-    }),
-  ],
+  plugins: [nextui()],
 };
 export default config;
